fb6acca814d933de0dad1fc8bec2788939dc94e8
additi123 <73571513+additi123@users.noreply.github.com>
2023-12-24 04:07:32 -0500

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,23 @@
+adjective = input("Enter a adjective: ")
+adjective1 = input("Enter another adjective: ")
+bird = input("Enter a type of bird: ")
+room = input("Enter a room in the house: ")
+verb_past = input("Enter a past tense verb: ")
+verb = input ("Enter a verb: ")
+relative = input("Enter a relative's name: ")
+noun = input("Enter a noun: ")
+liquid = input("Enter a liquid: ")
+verb2 = input("Enter a verb ending with ing: ")
+body_part = input("Enter a plural part of body: ")
+plural_noun = input("Enter a plural noun: ")
+verb3 = input("Enter another verb ending with ing: ")
+noun1 = input("Enter another noun: ")
+
+
+print("It was a "+ adjective + ", cold November day. I woke up to the " + adjective1 +
+      " smell of "+ bird + " roasting in the "+ room + " downstairs. I "+ verb_past +
+      " down the stairs to see if I could help " + verb + ' My Mom said , "See if '
+      + relative + ' needs a fresh ' + noun + '". So I carried a tray of glases full of "
+      + liquid + " into the " + verb2 + " room. When I got there, I couldn't believe my "
+      + body_part + "! There were " + plural_noun + " " + verb3 + " on the " + noun1 + "!"
+      )
